dcbf93ac71fc635ee7bd5dc7710f82011854967d
Wang, Dawei <daweiwang.gatekeeper@gmail.com>
2017-09-30 22:08:09 -0400

    Funnel functions

--- nifi/client.go
+++ nifi/client.go
@@ -973,3 +973,58 @@ func (c *Client) StartConnectionHand(connectionHand *ConnectionHand) error {
 	}
 	return nil
 }
+
+
+//Funnel
+type FunnelComponent struct {
+	Id            string    `json:"id,omitempty"`
+	ParentGroupId string    `json:"parentGroupId,omitempty"`
+	Position      Position `json:"position,omitempty"`
+}
+
+type Funnel struct {
+	Revision  Revision       `json:"revision"`
+	Component FunnelComponent `json:"component"`
+}
+
+func FunnelStub() *Funnel {
+	return &Funnel{
+		Component: FunnelComponent{
+			Position: Position{},
+		},
+	}
+}
+func (c *Client) CreateFunnel(funel *Funnel) error {
+	url := fmt.Sprintf("%s://%s/%s/processor-groups/%s/funnels",
+		c.HttpScheme, c.Config.Host, c.Config.ApiPath, funel.Component.ParentGroupId)
+	_, err := c.JsonCall("POST", url, funel, funel)
+	return err
+}
+func (c *Client) GetFunnel(funnelId string) (*Funnel, error) {
+	url := fmt.Sprintf("%s://%s/%s/funnels/%s",
+		c.HttpScheme, c.Config.Host, c.Config.ApiPath, funnelId)
+	funnel := FunnelStub()
+	code, err := c.JsonCall("GET", url, nil, &funnel)
+	if 404 == code {
+		return nil, fmt.Errorf("not_found")
+	}
+	if nil != err {
+		return nil, err
+	}
+	return funnel, nil
+}
+func (c *Client) UpdateFunnel(funnel *Funnel) error {
+	url := fmt.Sprintf("%s://%s/%s/funnels/%s",
+		c.HttpScheme, c.Config.Host, c.Config.ApiPath, funnel.Component.Id)
+	_, err := c.JsonCall("PUT", url, funnel, funnel)
+	if nil != err {
+		return err
+	}
+	return nil
+}
+func (c *Client) DeleteFunnel(funnel *Funnel) error {
+	url := fmt.Sprintf("%s://%s/%s/funnels/%s?version=%d",
+		c.HttpScheme, c.Config.Host, c.Config.ApiPath, funnel.Component.Id, funnel.Revision.Version)
+	_, err := c.JsonCall("DELETE", url, nil, nil)
+	return err
+}
